2b5100a8fc2027e38e5b7666b0f212d46b0534ea
Sumit Asok <sumitasok@gmail.com>
2014-08-07 14:52:14 +0530

    readily integrated and fixed issues

--- testdb.go
+++ testdb.go
@@ -10,21 +10,21 @@ import (
 )
 
 type TestDb struct {
-	mysql_username string
-	mysql_password string
-	dev_db_name    string
-	test_db_name   string
+	Mysql_username string
+	Mysql_password string
+	Dev_db_name    string
+	Test_db_name   string
 }
 
 func (tdb TestDb) Prepare() (*sql.DB, error) {
 	prepare_file, _ := os.Create("prepare.sh")
-	file_content := fmt.Sprintf("mysqldump -u %s -p%s --no-data %s > schema.sql\n\nmysql -u %s -p%s %s < schema.sql\n", tdb.mysql_username, tdb.mysql_password, tdb.dev_db_name, tdb.mysql_username, tdb.mysql_password, tdb.test_db_name)
+	file_content := fmt.Sprintf("mysqldump -u %s -p%s --no-data %s > schema.sql\n\nmysql -u %s -p%s %s < schema.sql\n", tdb.Mysql_username, tdb.Mysql_password, tdb.Dev_db_name, tdb.Mysql_username, tdb.Mysql_password, tdb.Test_db_name)
 	prepare_file.WriteString(file_content)
 
 	cmd := exec.Command("sh", "prepare.sh")
 	cmd.Output()
 
-	data_source_name := fmt.Sprintf("%s:%s@/%s?parseTime=true", tdb.mysql_username, tdb.mysql_password, "tdb.test_db_name")
+	data_source_name := fmt.Sprintf("%s:%s@/%s?parseTime=true", tdb.Mysql_username, tdb.Mysql_password, tdb.Test_db_name)
 	db, _ := sql.Open("mysql", data_source_name)
 
 	err := db.Ping()
@@ -33,7 +33,7 @@ func (tdb TestDb) Prepare() (*sql.DB, error) {
 		if strings.Contains(err.Error(), "Error 1049: Unknown database") == true {
 
 			fmt.Println("Caught ya! you need to create one test database for you!")
-			fmt.Println(fmt.Sprintf("run: mysql -u %s -p%s -e 'CREATE DATABASE %s'", tdb.mysql_username, tdb.mysql_password, tdb.test_db_name))
+			fmt.Println(fmt.Sprintf("run: mysql -u %s -p%s -e 'CREATE DATABASE %s'", tdb.Mysql_username, tdb.Mysql_password, tdb.Test_db_name))
 		}
 		return nil, err
 	}
